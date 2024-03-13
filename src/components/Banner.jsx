9b8a2e3cca51f389de409122c692ba2403391944
Grant Tracey <gtracey24@gmail.com>
2024-03-13 16:35:22 -0500

    Completed task 2

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+    let bannerTitle = "Orbit Report";
+    let instructions = "Take a gander at these incredible edible sattelites..."
+ 
+    return (
+       <div>
+          <header>{bannerTitle}</header>
+          <p>{instructions}</p>
+       </div>      
+    );
+ }
