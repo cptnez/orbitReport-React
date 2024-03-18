262ebe8ff679444046e4943f868ef0d52f6e9816
Grant Tracey <gtracey24@gmail.com>
2024-03-18 05:52:43 -0500

    Passing all tests. Need to finish status and formatting.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,14 +1,41 @@
+import satData from "./satData";
+
 const Table = ({ sat }) => {
+
+const tableName = sat.map(data => {
+  return <div key={data.id}>
+    <td>{data.name}</td>
+  </div>
+})
+
+const satType = sat.map(data => {
+  return <div key={data.id}>
+    <td>{data.type}</td>
+  </div>
+})
+
+const launchDate = sat.map(data => {
+  return <div key={data.id}>
+    <td>{data.launchDate}</td>
+  </div>
+})
+
+
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
      <tr>
-       <td>Row Data TBD</td>
+      {tableName}
+      {satType}
+      {launchDate}
      </tr>
      </tbody>
    </table>
