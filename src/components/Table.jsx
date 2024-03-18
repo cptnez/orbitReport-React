5ea59d2d9a6b707aa3077e0ab5f90959396f77cf
Grant Tracey <gtracey24@gmail.com>
2024-03-18 11:22:37 -0500

    Passing all tests. Formatted and status added.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,44 +1,60 @@
 import satData from "./satData";
 
 const Table = ({ sat }) => {
+  const tableName = sat.map((data) => {
+    return (
+      <tr key={data.id}>
+        <td>{data.name}</td>
+      </tr>
+    );
+  });
 
-const tableName = sat.map(data => {
-  return <div key={data.id}>
-    <td>{data.name}</td>
-  </div>
-})
+  const satType = sat.map((data) => {
+    return (
+      <tr key={data.id}>
+        <td>{data.type}</td>
+      </tr>
+    );
+  });
 
-const satType = sat.map(data => {
-  return <div key={data.id}>
-    <td>{data.type}</td>
-  </div>
-})
+  const launchDate = sat.map((data) => {
+    return (
+      <tr key={data.id}>
+        <td>{data.launchDate}</td>
+      </tr>
+    );
+  });
 
-const launchDate = sat.map(data => {
-  return <div key={data.id}>
-    <td>{data.launchDate}</td>
-  </div>
-})
+  const satStatus = sat.map((data) =>
+    data.operational === false ? (
+      <tr key={data.id}>
+        <td>Inactive</td>
+      </tr>
+    ) : (
+      <tr key={data.id}>
+        <td>Active</td>
+      </tr>
+    )
+  );
 
 
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Name</th>
-       <th>Type of Satellite</th>
-       <th>Launch Date</th>
-       <th>Status</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-      {tableName}
-      {satType}
-      {launchDate}
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <td>{tableName}</td>
+        <td>{satType}</td>
+        <td>{launchDate}</td>
+        <td>{satStatus}</td>
+      </tbody>
+    </table>
   );
 };
 
